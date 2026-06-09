39277f1efd44862d3b4de0275341c6d38848cd00
Riikq <tariqahmad0510@gmail.com>
2026-06-09 16:20:02 +0700

    init

---
+++ MLProject/modelling.py
@@ -0,0 +1,36 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+train_path = "pilgrimage_preprocessing/pilgrimage_train_preprocessed.csv"
+test_path = "pilgrimage_preprocessing/pilgrimage_test_preprocessed.csv"
+
+train_df = pd.read_csv(train_path)
+test_df = pd.read_csv(test_path)
+
+X_train = train_df.drop(columns=["target"])
+y_train = train_df["target"]
+X_test = test_df.drop(columns=["target"])
+y_test = test_df["target"]
+
+# mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+# mlflow.set_experiment("Pilgrimage Economic Impact")
+
+with mlflow.start_run(run_name="manual_run"):
+    mlflow.sklearn.autolog()
+
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    mlflow.sklearn.log_model(sk_model=model, artifact_path="model")
+
+    print(f"Mean Squared Error: {mse}")
+    print(f"R^2 Score: {r2}")
+
+mlflow.sklearn.save_model(model, "final_model")
